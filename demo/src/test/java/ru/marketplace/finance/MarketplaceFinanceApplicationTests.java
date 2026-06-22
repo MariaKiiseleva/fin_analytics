50e384826da054af8affd4ee46578e336092bc79
mkiiis <mkiiiseleva@mail.ru>
2026-06-22 17:12:15 +0300

    Настройка завершена

--- demo/src/test/java/ru/marketplace/finance/MarketplaceFinanceApplicationTests.java
+++ demo/src/test/java/ru/marketplace/finance/MarketplaceFinanceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package ru.marketplace.finance;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(properties = "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration,org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration")
-class DemoApplicationTests {
+class MarketplaceFinanceApplicationTests {
 
 	@Test
 	void contextLoads() {
